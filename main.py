b3035f3a895cef044a9dea7adcd2449622b154d5
Philip Bell <philhabell@gmail.com>
2017-02-13 14:23:04 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,90 @@
+import threading
+from threading import Lock
+from threading import Thread
+from time import sleep
+cv = threading.Condition()
+control = 0
+
+
+def func1():
+    global control
+    while True:
+        cv.acquire()
+        if control == 1:
+            
+            sleep(0.3)
+            print ("Thread: ",control)
+            control = 0
+        cv.notifyAll()
+        cv.release()
+
+def func2():
+    global control
+    while True:
+        cv.acquire()
+        if control == 2:
+            
+            sleep(0.3)
+            print ("Thread: ",control)
+            control = 0
+        cv.notifyAll()
+        cv.release()
+
+def func3():
+    global control
+    while True:
+        cv.acquire()
+        if control == 3:
+            sleep(0.3)
+            print ("Thread: ",control)
+            control = 0
+        cv.notifyAll()
+        cv.release()       
+
+def func4():
+    global control
+    while True:
+        cv.acquire()
+        if control == 4:
+            sleep(0.3)
+            print("Thread: ",control)
+            control = 0
+        cv.notifyAll()
+        cv.release()    
+
+def menu():
+    global control
+    while True:
+         if control == 0:   
+            print("Please enter which thread you would like to start with: \na) Thread 1\nb) Thread 2\nc) Thread 3\nd) Thread 4")
+            answer = input("Enter: ")
+            if (answer == "a"):
+                control = 1
+            elif (answer == "b"):
+                control = 2
+            elif (answer == "c"):
+                control = 3
+            elif (answer == "d"):
+                control = 4
+            else:
+                print ("Please enter a valid entry (a,b,c,d)")
+                control = 0
+
+t1 = Thread(target=func1,args=())
+t2 = Thread(target=func2,args=())
+t3 = Thread(target=func3,args=())
+t4 = Thread(target=func4,args=())
+t5 = Thread(target=menu,args=())
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+t5.start()
+t1.join()
+t2.join()
+t3.join()
+t4.join()
+t5.join()
+
+
+
