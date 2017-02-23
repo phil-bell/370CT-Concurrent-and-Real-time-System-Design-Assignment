b3a618b5fb438f69dbf9d9f87f31cde3ced439c3
Philip Bell <philhabell@gmail.com>
2017-02-23 01:39:16 +0000

    wheel 1 now check for problems

--- main.py
+++ main.py
@@ -2,89 +2,169 @@ import threading
 from threading import Lock
 from threading import Thread
 from time import sleep
-cv = threading.Condition()
+from sys import exit
+lock = threading.Condition()
 control = 0
+currentX = 1
+currentY = 1
+
+def mapCreate(w,h):
+    mapToOut = [[0 for i in range(w)] for j in range(h)]
+    return mapToOut
 
 
-def wheel1():
+"""
+0 = normal ground
+1 = rock (blocked)
+2 = hole (freewheeling)
+3 = sand (sinking)
+4 = water (Mission Success)
+"""
+def mapCheck(marsMap,wheelX,wheelY):
+    if marsMap[wheelX][wheelY] == 0:
+        return "clear"
+    elif marsMap[wheelX][wheelY] == 1:
+        return "rock"
+    elif marsMap[wheelX][wheelY] == 2:
+        return "hole"
+    elif marsMap[wheelX][wheelY] == 3:
+        return "sands"
+    elif marsMap[wheelX][wheelY] == 4:
+        return "water"
+    else:
+        return "Out of boundaries"
+    return 0
+
+
+def mainControl(marsMap):
     global control
     while True:
-        cv.acquire()
-        if control == 1:
+        lock.acquire()
+        print ("Main Control")
+        sleep(0.1)
+        control = 1
+        lock.notifyAll()
+        lock.release()
+
+"""
+Wheel positions:
+1-4
+2*5
+3-6
+currentX & currentY are the center of the rover
+"""
+def wheel1(marsMap):
+    global control
+    while True:
+        lock.acquire()
+
+        wheelX = currentX+1
+        wheelY = currentY+1 
+        print(mapCheck(marsMap,wheelX,wheelY))
+
+        if (control == 1):
+            sleep(0.1)
+            control = 2
+            print("Wheel: ",control)
             
-            sleep(0.3)
-            print ("Thread: ",control)
-            control = 0
-        cv.notifyAll()
-        cv.release()
+        lock.notifyAll()
+        lock.release()
 
-def wheel2():
+def wheel2(marsMap):
     global control
     while True:
-        cv.acquire()
-        if control == 2:
-            
-            sleep(0.3)
-            print ("Thread: ",control)
-            control = 0
-        cv.notifyAll()
-        cv.release()
+        lock.acquire() 
+        sleep(0.1)
+        control = 2
+        print ("Wheel: ",control)
+        lock.notifyAll()
+        lock.release()
 
-def wheel3():
+def wheel3(marsMap):
     global control
     while True:
-        cv.acquire()
-        if control == 3:
-            sleep(0.3)
-            print ("Thread: ",control)
-            control = 0
-        cv.notifyAll()
-        cv.release()       
+        lock.acquire() 
+        sleep(0.1)
+        control = 3
+        print("Wheel: ",control)
+        lock.notifyAll()
+        lock.release()       
 
-def wheel4():
+def wheel4(marsMap):
     global control
     while True:
-        cv.acquire()
-        if control == 4:
-            sleep(0.3)
-            print("Thread: ",control)
-            control = 0
-        cv.notifyAll()
-        cv.release()    
+        lock.acquire() 
+        sleep(0.1)
+        print("Wheel: ",control)
+        control = 4
+        lock.notifyAll()
+        lock.release()    
 
-def menu():
+def wheel5(marsMap):
     global control
     while True:
-         if control == 0:   
-            print("Which wheel would you like to check: \na) Wheel 1b) Thread 2\nc) Wheel 3\nd) Wheel 4")
-            answer = input("Enter: ")
-            if (answer == "a"):
-                control = 1
-            elif (answer == "b"):
-                control = 2
-            elif (answer == "c"):
-                control = 3
-            elif (answer == "d"):
-                control = 4
-            else:
-                print ("Please enter a valid entry (a,b,c,d)")
-                control = 0
+        lock.acquire() 
+        sleep(0.1)
+        control = 5
+        print("Wheel: ",control)
+        lock.notifyAll()
+        lock.release()  
 
-t1 = Thread(target=wheel1,args=())
-t2 = Thread(target=wheel2,args=())
-t3 = Thread(target=wheel3,args=())
-t4 = Thread(target=wheel4,args=())
-t5 = Thread(target=menu,args=())
+def wheel6(marsMap):
+    global control
+    while True:
+        lock.acquire() 
+        sleep(0.1)
+        control = 6
+        print("Wheel: ",control)
+        lock.notifyAll()
+        lock.release()  
+
+def menu(marsMap):
+    global control
+    lock.acquire() 
+
+    print("Which wheel would you like to check: \n1) Wheel 1\n2) Wheel 2\n3) Wheel 3\n4) Wheel 4\n5) Exit")
+    answer = int(input("Enter: "))
+    #print (type(answer))
+    if (answer == 1):
+        print ("1111")
+        control = 1
+        print (control)
+    elif (answer == 2):
+        control = 2
+    elif (answer == 3):
+         control = 3
+    elif (answer == 4):
+        control = 4
+    elif (answer == 5):
+        raise SystemExit()
+    else:
+        print ("Please enter a valid entry (a,b,c,d)")
+    control = 0
+    lock.release()
+
+marsMap = mapCreate(10,10)
+print (marsMap)
+
+t1 = Thread(target=mainControl,args=(marsMap,))
+t2 = Thread(target=wheel1,args=(marsMap,))
+t3 = Thread(target=wheel2,args=(marsMap,))
+t4 = Thread(target=wheel3,args=(marsMap,))
+t5 = Thread(target=wheel4,args=(marsMap,))
+t6 = Thread(target=menu,args=(marsMap,))
 t1.start()
 t2.start()
 t3.start()
 t4.start()
 t5.start()
+#t6.start()
 t1.join()
 t2.join()
 t3.join()
 t4.join()
 t5.join()
+#t6.join()
 
 
 
